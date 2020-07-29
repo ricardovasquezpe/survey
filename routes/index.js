dc52f844560e84fb5aa984839710ca8d13759fe4
misaelriojas <misael.riojas@fulltimeforce.com>
2020-07-29 03:05:58 -0500

    adde post and validations

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,47 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var otp = require('../models/otp.js');
 
+/// TODO: REFACTOR [INPUTQTY]
+const inputQty = 4;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-    { title: 'OTP Validation' }
-  );
+    res.render('index', { title: 'OTP Validation' });
 });
 
-router.get('/searchOtp', function(req, res, next){
-  var query = {
-      otp: req.query.otp
-  };
-  otp.findOne(query).exec(function(err, otpRes){
-    res.json(
-      {"status" : (otpRes == null || otpRes.used) ? false : true}
-    );
+router.post('/searchOtp', function(req, res, next) {
+
+    let otpNumber = req.body.code;
+    console.log(!isNaN(otpNumber));
+    if (!isNaN(otpNumber) && otpNumber.toString().length == inputQty) {
+        let otpModel = {
+            otp: otpNumber
+        };
+
+        otp.findOne(otpModel).exec(function(err, otpRes) {
+
+            if (err)
+                return res.json({ "available": false });
+
+            let availableNumber = (otpRes == null || !otpRes.used);
+
+            if (availableNumber) {
+                let doc = new otp({
+                    otp: otpModel.otp,
+                    used: true
+                });
+                doc.save(function(err, doc) {
+                    if (err) return console.error(err);
+                    console.log("Document inserted succussfully!");
+                });
+            }
+            res.json({ "status": availableNumber ? true : false });
+            return;
+        });
+    } else
+        res.json({ "error": "This value is not valid" });
     return;
-  });
 });
+
 
 module.exports = router;
